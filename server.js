4308809a35a60a5da69dd61f5dc140869ce13c07
LSDiego21 <148590597+LSDiego21@users.noreply.github.com>
2026-02-21 07:37:44 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -19,13 +19,35 @@ app.get('/', (req, res) => {
 // ==========================================
 // 2. ENDPOINT RESTful (Para JSON)
 // ==========================================
-app.get('/api/monitoreo-sargazo', (req, res) => {
-    res.status(200).json({
-        success: true,
-        version_api: "1.0-REST",
-        conteo_alertas: registrosSargazo.length,
-        alertas: registrosSargazo
-    });
+// ==========================================
+// 2. ENDPOINT RESTful (Consumiendo al IMTA)
+// ==========================================
+app.get('/api/monitoreo-sargazo', async (req, res) => {
+    try {
+        // 1. CONSUMIR: Tu servidor sale a internet y hace ping al servidor del IMTA
+        const respuestaIMTA = await fetch('https://sargazo.imta.mx/index.html');
+        
+        // Verificamos si el servidor del gobierno nos contestó correctamente
+        const estatusGobierno = respuestaIMTA.ok ? "En línea y conectado ✅" : "Fuera de servicio ❌";
+
+        // 2. EXPONER: Le entregamos la información a tu cliente
+        res.status(200).json({
+            success: true,
+            version_api: "2.0-CONSUMO-REAL",
+            fuente_oficial: "Instituto Mexicano de Tecnología del Agua (IMTA)",
+            url_consumida: "https://sargazo.imta.mx/index.html",
+            estado_servidor_imta: estatusGobierno,
+            conteo_alertas: registrosSargazo.length,
+            alertas: registrosSargazo
+        });
+
+    } catch (error) {
+        // Si el internet falla o la página del IMTA se cae, lo reportamos
+        res.status(500).json({ 
+            success: false, 
+            error: "Fallo al conectar con el servidor satelital del IMTA" 
+        });
+    }
 });
 
 // ==========================================
