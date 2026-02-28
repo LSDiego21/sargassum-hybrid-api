93638b26a8700359336c643c9fb61c00052dc484
LSDiego21 <148590597+LSDiego21@users.noreply.github.com>
2026-02-27 22:47:58 -0500

    Delete database.js

--- database.js
+++
@@ -1,8 +0,0 @@
-const registrosSargazo = [
-    { municipio: "Solidaridad", playa: "Playa del Carmen Centro", nivel_inundacion: "MEDIO", biomasa_estimada_tons: 650 },
-    { municipio: "Benito Juarez", playa: "Cancún Playa Coral", nivel_inundacion: "ALTO", biomasa_estimada_tons: 1200 },
-    { municipio: "Tulum", playa: "Playa Ruinas", nivel_inundacion: "BAJO", biomasa_estimada_tons: 150 },
-    { municipio: "Puerto Morelos", playa: "Ventana al Mar", nivel_inundacion: "MEDIO", biomasa_estimada_tons: 400 }
-];
-
-module.exports = { registrosSargazo };
